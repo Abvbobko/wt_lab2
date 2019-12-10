195c106102d2d9d5dd1c694dd96b05a7dac2e7a9
Alexei Bobko <41486012+Abvbobko@users.noreply.github.com>
2019-12-10 17:03:52 +0300

    add xsd xml validation

--- src/company/Main.java
+++ src/company/Main.java
@@ -1,8 +1,20 @@
 package company;
 
+
+import service.Service;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Service a = new Service();
+
+        if (a.validate("C:\\Users\\hp\\Desktop\\WT_LAB2\\wt_lab2\\resources\\airlineData.xsd",
+                "C:\\Users\\hp\\Desktop\\WT_LAB2\\wt_lab2\\resources\\airlineData.xml")) {
+            System.out.println("yes");
+        }
+        else {
+            System.out.println("no");
+        }
     }
 }
