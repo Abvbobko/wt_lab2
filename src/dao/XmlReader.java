5ee974e4f5b828cccace7d37e5a8bf6935ee030d
Alexei Bobko <41486012+Abvbobko@users.noreply.github.com>
2019-12-15 16:06:45 +0300

    implement xml parser

--- src/dao/XmlReader.java
+++ src/dao/XmlReader.java
@@ -1,6 +1,6 @@
 package dao;
 
-//import dao.parser.*;
+import dao.parser.*;
 import beans.*;
 import exception.DataSourceException;
 import org.apache.logging.log4j.LogManager;
@@ -13,5 +13,65 @@ import java.io.IOException;
 import java.util.List;
 
 public class XmlReader {
+    private static final Logger logger = LogManager.getLogger();
+    private UserParser userParser;
+    private FlightParser flightParser;
+    private PilotParser pilotParser;
+    private PlaneParser planeParser;
+    private TicketParser ticketParser;
 
+    private List<User> users;
+    private List<Flight> flights;
+    private List<Plane> planes;
+    private List<Pilot> pilots;
+    private List<Ticket> tickets;
+
+    public XmlReader(String file) throws DataSourceException {
+        logger.info("Xml reader starts parsing file - " + file + ".");
+        userParser = new UserParser(file);
+        flightParser = new FlightParser(file);
+        pilotParser = new PilotParser(file);
+        planeParser = new PlaneParser(file);
+        ticketParser = new TicketParser(file);
+    }
+
+    public List<User> getUsers() {
+        if (users == null) {
+            users = userParser.getUsers();
+            logger.info("User list is ready.");
+        }
+        return users;
+    }
+
+    public List<Flight> getFlights() {
+        if (flights == null) {
+            flights = flightParser.getFlights();
+            logger.info("Flight list is ready.");
+        }
+        return flights;
+    }
+
+    public List<Pilot> getPilots() {
+        if (pilots == null) {
+            pilots = pilotParser.getPilots();
+            logger.info("Pilot list is ready.");
+        }
+        return pilots;
+    }
+
+    public List<Plane> getPlanes() {
+        if (planes == null) {
+            planes = planeParser.getPlanes();
+            logger.info("Plane list is ready.");
+        }
+        return planes;
+    }
+
+    public List<Ticket> getTickets() {
+        if (tickets == null) {
+            tickets = ticketParser.getTickets();
+            logger.info("Ticket list is ready.");
+        }
+        return tickets;
+    }
 }
